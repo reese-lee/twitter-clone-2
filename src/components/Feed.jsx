dcd7b059b7886ea7a8f29e43f61c1904c4ad66b8
Neil Bateman and Reese Lee <neil@>
2019-07-02 10:15:56 -0700

    add ticket list objects to be looped

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -3,6 +3,8 @@ import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import FilterFeed from './FilterFeed';
+import Ticket from './Ticket';
+
 
 const useStyles = makeStyles({
   card: {
@@ -22,6 +24,24 @@ const useStyles = makeStyles({
   },
 });
 
+var masterTicketList = [
+  {
+    names: 'Thato and Haley',
+    location: '3A',
+    issue: 'Firebase won\'t save record. Halp.'
+  },
+  {
+    names: 'Sleater and Kinney',
+    location: '4B',
+    issue: 'Fox image not displaying on page, can only see duck?'
+  },
+  {
+    names: 'Imani & Jacob',
+    location: '9F',
+    issue: 'Donkey picture not displaying on hover in Zoology app. :('
+  }
+];
+
 export default function SimpleCard() {
   const classes = useStyles();
   // const bull = <span className={classes.bullet}>•</span>;
@@ -29,11 +49,18 @@ export default function SimpleCard() {
   return (
 
     <Card className={classes.card}>
-
-      <FilterFeed></FilterFeed>
       <CardContent>
-        <h1>BlaBla</h1>
-        <p>al;disjfl;kasdjfl;akdsjgl;kadjfgl;kadjffg Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <div>
+          {masterTicketList.map((ticket, index) =>
+            <Ticket names={ticket.names}
+              location={ticket.location}
+              issue={ticket.issue}
+              key={index}/>
+          )}
+        </div>
+        <FilterFeed></FilterFeed>
+
+
       </CardContent>
     </Card>
   );
